769b01d4b274060c4f1c98d4357a12bc14d024ca
Unknown <izaro.garcia.uribe@gmail.com>
2019-02-06 18:57:41 +0100

    Crear proyecto + Pod Firebase
    Se ha instalado el pod de firebase y ya tenemos Workspace

---
+++ BIWALK/BIWALK/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BIWALK
+//
+//  Created by  on 6/2/19.
+//  Copyright © 2019 Izaisa. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
